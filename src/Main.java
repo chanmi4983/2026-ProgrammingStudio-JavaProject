bcc5c75d1587680231a8b523e3e343908dad8a70
zzanmi <chanmi4983@naver.com>
2026-05-01 19:51:14 +0900

    add Main: select Menu

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        BookManager manager = new BookManager();
+
+        while (true) {
+            System.out.println("\n===== Book Management Menu =====");
+            System.out.println("1. Add Book");
+            System.out.println("2. List Books");
+            System.out.println("3. Update Book");
+            System.out.println("4. Delete Book");
+            System.out.println("0. Exit");
+            System.out.print("Select menu: ");
+
+            int menu = s.nextInt();
+
+            if (menu == 1) {
+                manager.addBook();
+            } else if (menu == 2) {
+                manager.listBooks();
+            } else if (menu == 3) {
+                manager.updateBook();
+            } else if (menu == 4) {
+                manager.deleteBook();
+            } else if (menu == 0) {
+                System.out.println("Program exited.");
+                break;
+            } else {
+                System.out.println("Invalid menu.");
+            }
+        }
+    }
+}
